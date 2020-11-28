c75893e03f08b91168f4206d642062276e8f021f
Julio Romero <julioale04031981@gmail.com>
2020-11-28 17:15:52 -0300

    todo app

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>ToDos</h1>
+  <todo-form />
+  <todo-list />
+</template>
+
+<script>
+import { provide, ref, watchEffect } from 'vue'
+import TodoForm from './TodoForm.vue'
+import TodoList from './TodoList.vue'
+export default {
+    components: { TodoForm, TodoList },
+    setup() {
+        const todos = ref([])
+        provide('todos', todos)
+
+        if(localStorage.getItem('todos')){
+            todos.value = JSON.parse(localStorage.getItem('todos'))
+        }
+
+        watchEffect(() => {
+            localStorage.setItem('todos', JSON.stringify(todos.value))
+        })
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TodoForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <form @submit.prevent="formulario">
+        <input 
+            type="text"
+            class="form-control my-3"
+            placeholder="Ingrese tarea"
+            v-model.trim="texto"
+        >
+  </form>
+</template>
+
+<script>
+import { inject, ref } from 'vue'
+export default {
+    setup() {
+        const todos = inject('todos')
+        const texto = ref('')
+        
+        const formulario = () => {
+            if(texto.value === '') {
+                console.log('Esta vacio')
+                return
+            }
+            const todo = {
+                texto: texto.value,
+                estado: false,
+                id: Date.now()
+            }
+            todos.value.push(todo)
+            texto.value = ''
+        }
+
+        return {formulario, texto}
+    }
+}
+</script>
+
+<style>
+
+</style>
